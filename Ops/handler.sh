f04baa996044993dac2d2e55d5b0c34af2f22f32
veia-mac <917971502@qq.com>
2018-12-22 13:30:28 +0800

    add ops sh

---
+++ Ops/handler.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+case "$1" in
+    "node")
+        case "$2" in
+            "start")
+            ansible -i hosts all -m shell -a "nohup /root/node_exporter-0.16.0.linux-amd64/node_exporter >/dev/null 2>&1 &"
+            ;;
+            *)
+            echo "no action"
+            ;;
+        esac
+        ;;
+    "kafka")
+        case "$2" in
+            "start")
+            ansible -i hosts kafka -m shell -a "nohup /root/kafka_2.12-2.0.0/bin/kafka-server-start.sh config/server.properties >/dev/null 2>&1 &"
+            ;;
+            "stop")
+            ansible -i hosts kafka -m shell -a "nohup /root/kafka_2.12-2.0.0/bin/kafka-server-stop.sh >/dev/null 2>&1 &"
+            ;;
+            "fstop")
+            ansible -i hosts kafka -m shell -a "ps -ef |grep kafka |awk '{print \$2}'|xargs kill -9"
+            ;;
+            "status")
+            ansible -i hosts kafka -m shell -a "ps -ef |grep kafka |awk '{print \$2}'"
+            ;;
+            *)
+            echo "no action"
+            ;;
+        esac
+        ;;
+    "zoo")
+        case "$2" in
+            "start")
+            ansible -i hosts kafka -m shell -a "/root/zookeeper-3.4.13/bin/zkServer.sh start"
+            ;;
+            "stop")
+            ansible -i hosts kafka -m shell -a "/root/zookeeper-3.4.13/bin/zkServer.sh stop"
+            ;;
+            "status")
+            ansible -i hosts kafka -m shell -a "/root/zookeeper-3.4.13/bin/zkServer.sh status"
+            ;;
+            *)
+            echo "no action"
+            ;;
+        esac
+        ;;
+    *)
+        echo "no command"
+        ;;
+esac
