cbe029bcb172e4217a91ec1ed6a15852617ed4b7
Yunchao Wang <wangyunchao@sjtu.edu.cn>
2018-12-31 15:45:18 +0800

    add analyze script

--- PythonScript/produceData.sh
+++ PythonScript/produceData.sh
@@ -1,1 +1,1 @@
-python generateData.py --fileAmount 2 --timeLength 120 --dropTime 4 --dropInterval 10 --dropFileAmount 1 --messageType 2 --enableNoise 0
+python generateData.py --fileAmount 2 --timeLength 120 --dropTime 4 --dropInterval 10 --dropFileAmount 1 --messageType 2 --enableNoise 0 --feedCodeStartId 1000
