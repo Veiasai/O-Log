723ee3de3e83aa2aa91e979c48de361c9be9e6c0
Yunchao Wang <wangyunchao@sjtu.edu.cn>
2019-01-01 16:24:43 +0800

    add xml conf

---
+++ rdkafka/test/xmlConfTest.cpp
@@ -0,0 +1,37 @@
+#include "../lib/tinyxml.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    TiXmlDocument doc("../cppStream.xml");
+    if(!doc.LoadFile())
+    {
+        cerr<<"Failed to Load file." << endl;
+        return 1;
+    }
+    TiXmlElement* root = doc.FirstChildElement();
+    if(root == NULL)
+    {
+        cerr << "No root element." << endl;
+        return 1;
+    }
+    for(TiXmlElement* elem = root->FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement())
+    {
+        for(TiXmlElement* conf = elem->FirstChildElement(); conf != NULL; conf = conf->NextSiblingElement())
+        {
+            if (conf->FirstChildElement() == NULL)
+            {
+                cout << conf->GetText() << endl;
+            }
+            else
+            {
+                for (TiXmlElement *entry = conf->FirstChildElement(); entry != NULL; entry = entry->NextSiblingElement())
+                {
+                    cout << entry->GetText() << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
