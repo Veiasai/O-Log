b1160ee9089e4fc5b5c8428c3088678a0aab7123
Yunchao Wang <wangyunchao@sjtu.edu.cn>
2019-01-01 08:53:30 +0800

    cout timestamp

--- rdkafka/myConsumer.cpp
+++ rdkafka/myConsumer.cpp
@@ -142,6 +142,13 @@ void MyConsumer::consume()
 }
 
 
+int64_t getCurrentTime()      //直接调用这个函数就行了，返回值最好是int64_t，long long应该也可以
+{    
+    struct timeval tv;    
+    gettimeofday(&tv,NULL);    //该函数在sys/time.h头文件中
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000;    
+}
+
 void MyConsumer::msg_consume(RdKafka::Message* message, void* opaque) {
     switch (message->err()) {
         case RdKafka::ERR__TIMED_OUT:
