aeba4a6fa488d632046d27d365c7601ff9444515
Yunchao Wang <wangyunchao@sjtu.edu.cn>
2019-01-05 14:33:57 +0800

    support multi consumer&producer

--- rdkafka/main.cpp
+++ rdkafka/main.cpp
@@ -8,24 +8,47 @@ int main(){
     HandlerConfs handlerConfs= ConfLoader::load("rdkafka/conf/cppStream.xml");
     if (handlerConfs.first.size()==0 || handlerConfs.second.size()==0)
     {
-        std::cerr << handlerConfs.first.size() << std::endl
-                  << handlerConfs.second.size() << std::endl;
         std::cerr << "Load Conf Error." << std::endl;
         return 1;
     }
 
-    MyConsumer myConsumer(handlerConfs.first[0]);
-    MyProducer myProducer(handlerConfs.second[0]);
+    vector<MyConsumer> myConsumers;
+    for (auto& handlerConf : handlerConfs.first)
+    {
+        myConsumers.emplace_back(myConsumer(handlerConf));
+    }
 
-    Processor* dp = new Demo_processor();
+    vector<MyProducer> myProducers;
+    for (auto& handlerConf : handlerConfs.second)
+    {
+        myProducers.emplace_back(myProducer(handlerConf));
+    }
+
+    Processor *dp = new Demo_processor();
+
+    for (int i = 0; i < myConsumers.size(); i++)
+    {
+        if (!myProducers.empty())
+        {
+            myConsumers[i].setProducer(&myProducers[i%myProducers.size()]);
+        }
+        myConsumers[i].setProcessor(dp);
+        myConsumers[i].subscribe();
+    }
     // dp->exec("{\"@timestamp\":1545893491.708491, \"log_time\":\"1545893491503\", \"MessageType\":\"LIMon_StatisticsFeed\", \"detail\":\"rb1000,2306359659839382000,6414.451840,6414.451840,6468.784659,6468.784659,CLOSING_PRICE_PREVIOUS_TRADING_DAY,6414.451840,1,6414.451840,1,0,6468.784659,523,1545893491502980951,7115.663125,5821.906193,54465099783379212,1545893491500000000\"}");
-    myConsumer.setProcessor(dp);
-    myConsumer.setProducer(&myProducer);
-    myProducer.produce("init");
-    myConsumer.subscribe();
+    // myConsumer.setProcessor(dp);
+    // myConsumer.setProducer(&myProducer);
+    // myProducer.produce("init");
+    // myConsumer.subscribe();
     while(run){
-        myConsumer.consume();
-        myProducer.poll(0);
+        for (auto& myConsumer : myConsumers)
+        {
+            myConsumer.consume();
+        }
+        for (auto& myProducer : myProducers)
+        {
+            myProducer.poll(0);
+        }
     }
 }
 
