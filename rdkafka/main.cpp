41d0baeb215d8434337c0122d39edc5c51bd242e
Yunchao Wang <wangyunchao@sjtu.edu.cn>
2019-01-05 16:07:30 +0800

    debug

--- rdkafka/main.cpp
+++ rdkafka/main.cpp
@@ -31,11 +31,14 @@ int main(){
     {
         if (!myProducers.empty())
         {
+            std::cout << "3.1" << endl;
             myConsumers[i].setProducer(&(myProducers[i%myProducers.size()]));
+            std::cout << "3.2" << endl;
         }
         myConsumers[i].setProcessor(dp);
+        std::cout << "3.3" << endl;
         myConsumers[i].subscribe();
-        std::cout << "3" << endl;
+        std::cout << "3.4" << endl;
     }
     std::cout << "init success" << std::endl;
     // dp->exec("{\"@timestamp\":1545893491.708491, \"log_time\":\"1545893491503\", \"MessageType\":\"LIMon_StatisticsFeed\", \"detail\":\"rb1000,2306359659839382000,6414.451840,6414.451840,6468.784659,6468.784659,CLOSING_PRICE_PREVIOUS_TRADING_DAY,6414.451840,1,6414.451840,1,0,6468.784659,523,1545893491502980951,7115.663125,5821.906193,54465099783379212,1545893491500000000\"}");
