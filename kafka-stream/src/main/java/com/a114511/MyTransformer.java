03b061db7fb1ca9f0f2f0ca7d6c5e12ae458d465
Yu <1451168305@qq.com>
2019-01-14 20:01:08 +0800

    test

--- kafka-stream/src/main/java/com/a114511/MyTransformer.java
+++ kafka-stream/src/main/java/com/a114511/MyTransformer.java
@@ -27,7 +27,7 @@ public class MyTransformer implements Transformer<String, String, KeyValue<Strin
     public void init(ProcessorContext context) {
         // Not needed.
 
-        int productNum = 1000;
+        int productNum = 1500;
         int productIdStart = 4000;
         for (int i = 0; i < productNum; ++i) {
             products.add("rb" + String.valueOf(productIdStart + i));
