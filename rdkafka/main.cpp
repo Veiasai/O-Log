e81f9369381eedd7c5799af656d88cc65c4b6853
Yunchao Wang <wangyunchao@sjtu.edu.cn>
2019-01-17 16:48:24 +0800

    update drone

--- rdkafka/main.cpp
+++ rdkafka/main.cpp
@@ -4,12 +4,12 @@
 bool run = true;
 
 int main(){
-    HandlerConfs handlerConfs= ConfLoader::load("rdkafka/conf/cppStream.xml");
-    // if (handlerConfs.first.size()==0 || handlerConfs.second.size()==0)
-    // {
-    //     std::cerr << "Load Conf Error." << std::endl;
-    //     return 1;
-    // }
+    HandlerConfs handlerConfs= ConfLoader::load("conf/cppStream.xml");
+    if (handlerConfs.first.size()==0 || handlerConfs.second.size()==0)
+    {
+        std::cerr << "Load Conf Error." << std::endl;
+        return 1;
+    }
     EventLoop eventLoop;
     eventLoop.setMyConsumer(handlerConfs.first[0]);
     eventLoop.setMyProducer(handlerConfs.second[0]);
