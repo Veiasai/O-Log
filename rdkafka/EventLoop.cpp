457ab04521cd6fc53e3ee81de9fec688757d31cb
Yunchao Wang <wangyunchao@sjtu.edu.cn>
2019-01-17 17:13:21 +0800

    fix

--- rdkafka/EventLoop.cpp
+++ rdkafka/EventLoop.cpp
@@ -56,22 +56,19 @@ void EventLoop::run()
 {
     while(true)
     {
-        std::cout << "in loop" << std::endl;
         RdKafka::Message *message = myConsumer->consume();
-        std::cout << 1 << std::endl;
-        std::string messageStr(static_cast<const char *>(message->payload()));
-        std::cout << 2 << std::endl;
-        for(auto& processor : processors)
+        if (message != NULL)
         {
-            // for now exec use string as input, you should change it to RdKafka::Message*, and don't forget to delete it.
-            processor->exec(messageStr);
-            std::cout << 3 << std::endl;
-            Pro_res res = processor->getResult();
-            std::cout << 4 << std::endl;
-            if (res.code != Status::OK){
-                myProducer->produce(res.json);
+            std::string messageStr(static_cast<const char *>(message->payload()));
+            for(auto& processor : processors)
+            {
+                // for now exec use string as input, you should change it to RdKafka::Message*, and don't forget to delete it.
+                processor->exec(messageStr);
+                Pro_res res = processor->getResult();
+                if (res.code != Status::OK){
+                    myProducer->produce(res.json);
+                }
             }
-            std::cout << 5 << std::endl;
         }
         myProducer->poll(0);
     }
